014c0937de81c9192d5826b7a5c124e2daa4dd4d
Rustam S <blakfoh@gmail.com>
2023-06-08 14:35:26 +0600

    добавил алерты для UserName & Password

--- RegistrationFlow/LoginFlowViewController.swift
+++ RegistrationFlow/LoginFlowViewController.swift
@@ -14,6 +14,30 @@ final class LoginFlowViewController: UIViewController {
         
     }
     
+    @IBAction func forgotUserNameButton() {
+        showAlert(title: "Your User Name:", message: "Rustam")
+    }
     
+    @IBAction func forgotPasswordButton() {
+        showAlert(title: "Your Password:", message: "XOXO")
+    }
 }
 
+// MARK: - UIAlertController
+private extension LoginFlowViewController {
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .actionSheet
+        )
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+//            self.textField.text = ""
+        }
+        
+        alert.addAction(okAction)
+
+        present(alert, animated: true)  // отображение экрана
+    }
+}
