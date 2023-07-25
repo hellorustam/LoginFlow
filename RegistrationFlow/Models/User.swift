45369fd523819654c7e6b0659b4cb88daa74d026
Rustam S <blakfoh@gmail.com>
2023-07-25 17:08:31 +0300

    Поправил модель данных

--- RegistrationFlow/Models/User.swift
+++ RegistrationFlow/Models/User.swift
@@ -8,23 +8,17 @@
 struct User {
     let login: String
     let password: String
-    let person: Person
-    
     let aims: String
     let coolStory: String
+    let person: Person
     
     static func getUser() -> User {
        User(
             login: "Rustam",
             password: "XOXO",
-            person: Person(
-                avatar: "myProfilePicture",
-                fullName: "Rustam Samiev",
-                about: "🤖 I'm IT enthusiast and I like to create Apps with Swift",
-                facts: "I like to CODE,\nDANCE & CHOCOLATE"
-            ),
             aims: "Learn how to cook 28 meals & planning great travel trips",
-            coolStory: "🏕️\nOnce my friends and I gathered at the lake Kupavna. We grilled kebabs and sang songs all night long. But when we went to bed, our kebab was stolen by the 🦫 andatra."
+            coolStory: "🏕️\nOnce my friends and I gathered at the lake Kupavna. We grilled kebabs and sang songs all night long. But when we went to bed, our kebab was stolen by the 🦫 andatra.",
+            person: Person.getPerson()
         )
     }
 }
@@ -34,4 +28,13 @@ struct Person {
     let fullName: String
     let about: String
     let facts: String
+    
+    static func getPerson() -> Person {
+        Person(
+            avatar: "myProfilePicture",
+            fullName: "Rustam Samiev",
+            about: "🤖 I'm IT enthusiast and I like to create Apps with Swift",
+            facts: "I like to CODE,\nDANCE & CHOCOLATE"
+        )
+    }
 }
